01c1679dc1f698d41daae95b7de1e3d31394c987
ligerzero459 <ligerzero459@gmail.com>
2011-10-21 04:37:00 -0500

    ObjTests

---
+++ CodeTests/ObjTests/js/TowerDefense/MapCreator.js
@@ -0,0 +1,14 @@
+/*
+/	MapCreator.js
+/	Creates level map for tower defense game
+/	Author: ligerzero459
+*/
+
+MapCreator = function ( TH, TW, MH, MW ) {
+
+	this.TILE_H = TH;
+	this.TILE_W = TW;
+	this.MAP_H = MH;
+	this.MAP_W = MW;
+	
+}
