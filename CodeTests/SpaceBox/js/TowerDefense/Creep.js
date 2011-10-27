6ee4173534da542f76592ef582a9321b137affb6
ligerzero459 <ligerzero459@gmail.com>
2011-10-27 16:40:05 -0500

    No creep was displayed. Reverting code to last working version

--- CodeTests/SpaceBox/js/TowerDefense/Creep.js
+++ CodeTests/SpaceBox/js/TowerDefense/Creep.js
@@ -1,21 +1,17 @@
 var Creep = Creep || {};
 
 Creep.initialize = function () {
+
 	var health;
 	
 	this.health = health;
 	this.x = xPathArray[xLength];
 	this.y = yPathArray[yLength];
 	
-	this.load();
-}
-
-Creep.load = function () {
-	
 	this.material = new THREE.MeshLambertMaterial ( { color: 0x00FFFF } );
 	this.geometry = new THREE.SphereGeometry( 100, 20, 20 );
 	this.geometry.computeTangents();
 	this.mesh = new THREE.Mesh ( geometry, material );
 	this.mesh.position.set( this.x, this.y, 0 );
-	scene.add( this.mesh );
+	
 }
