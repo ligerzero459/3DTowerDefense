7ec541c3e470900b2232569ccf25c5efa6fe5643
ligerzero459 <ligerzero459@gmail.com>
2011-10-26 22:37:21 -0500

    TowerDefense tests

---
+++ js/Creep.js
@@ -0,0 +1,23 @@
+var Creep = Creep || {};
+
+Creep.initialize = function () {
+	var self = this;
+	var material, geometry, mesh;
+	
+	this.health = health;
+	this.x = xPathArray[xLength];
+	this.y = yPathArray[yLength];
+	
+	this.load();
+}
+
+Creep.load = function () {
+	material = new THREE.MeshLambertMaterial ( { color: 0x00FFFF } );
+	geometry = new THREE.SphereGeometry( 100, 20, 20 );
+	geometry.computeTangents();
+	mesh = new THREE.Mesh ( geometry, material );
+	mesh.position.set( this.x, this.y, 0 );
+	
+	scene.add( mesh );
+	this.mesh = mesh;
+}
