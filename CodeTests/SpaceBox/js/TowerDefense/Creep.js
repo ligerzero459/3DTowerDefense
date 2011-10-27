7caa1f1831c3d138bdba7495e075c74e90c448d5
ligerzero459 <ligerzero459@gmail.com>
2011-10-27 16:34:47 -0500

    Continuing OOP tests

--- CodeTests/SpaceBox/js/TowerDefense/Creep.js
+++ CodeTests/SpaceBox/js/TowerDefense/Creep.js
@@ -11,14 +11,11 @@ Creep.initialize = function () {
 }
 
 Creep.load = function () {
-	var material, geometry, mesh;
 	
-	material = new THREE.MeshLambertMaterial ( { color: 0x00FFFF } );
-	geometry = new THREE.SphereGeometry( 100, 20, 20 );
-	geometry.computeTangents();
-	mesh = new THREE.Mesh ( geometry, material );
-	mesh.position.set( this.x, this.y, 0 );
-	
-	scene.add( mesh );
-	this.mesh = mesh;
+	this.material = new THREE.MeshLambertMaterial ( { color: 0x00FFFF } );
+	this.geometry = new THREE.SphereGeometry( 100, 20, 20 );
+	this.geometry.computeTangents();
+	this.mesh = new THREE.Mesh ( geometry, material );
+	this.mesh.position.set( this.x, this.y, 0 );
+	scene.add( this.mesh );
 }
