e60fd76a8a5983e2070db9210a1b420d8e21b345
ligerzero459 <ligerzero459@gmail.com>
2011-10-29 12:51:37 -0500

    ...

--- CodeTests/SpaceBox/js/TowerDefense/Creep.js
+++ CodeTests/SpaceBox/js/TowerDefense/Creep.js
@@ -28,85 +28,82 @@ Creep.create = function () {
 }
 
 Creep.update = function() {
-	if (this.totalCreeps > 0)
+	for (var i in this.creeps)
 	{
-		for (var i in this.creeps)
-		{
-				
-			if (this.creeps[i].position.x != xPathArray[this.creepWaypoint[i]])
-			{
-				if (this.creeps[i].position.x > xPathArray[this.creepWaypoint[i]] && this.MOVE_E[i] == false)
-				{
-					this.creeps[i].position.x -= 1.7;
-					this.MOVE_W[i] = true;
-				}
-				else if (this.creeps[i].position.x < xPathArray[this.creepWaypoint[i]] && this.MOVE_W[i] == false)
-				{
-					this.creeps[i].position.x += 1.7;
-					this.MOVE_E[i] = true;
-				}
-				else
-				{
-					this.creeps[i].position.x = xPathArray[this.creepWaypoint[i]];
-					
-					if (this.creeps[i].position.x == 3 && this.creeps[i].position.y == 1)
-					{
-						this.creepWaypoint[i] = xPathArray.length - 2;
-						this.creeps[i].position.x = xPathArray[xPathArray.length - 1];
-						this.creeps[i].position.y = yPathArray[xPathArray.length - 1];
-						this.MOVE_E[i] = false;
-						this.MOVE_W[i] = false;
-					}
-					else
-					{
-						this.creepWaypoint[i]--;
-						this.MOVE_E[i] = false;
-						this.MOVE_W[i] = false;
-					}
-				}
-			}
 			
-			if (this.creeps[i].position.y != yPathArray[this.creepWaypoint[i]])
+		if (this.creeps[i].position.x != xPathArray[this.creepWaypoint[i]])
+		{
+			if (this.creeps[i].position.x > xPathArray[this.creepWaypoint[i]] && this.MOVE_E[i] == false)
 			{
-				if (this.creeps[i].position.y > yPathArray[this.creepWaypoint[i]] && this.MOVE_N[i] == false)
+				this.creeps[i].position.x -= 1.7;
+				this.MOVE_W[i] = true;
+			}
+			else if (this.creeps[i].position.x < xPathArray[this.creepWaypoint[i]] && this.MOVE_W[i] == false)
+			{
+				this.creeps[i].position.x += 1.7;
+				this.MOVE_E[i] = true;
+			}
+			else
+			{
+				this.creeps[i].position.x = xPathArray[this.creepWaypoint[i]];
+				
+				if (this.creeps[i].position.x == 3 && this.creeps[i].position.y == 1)
 				{
-					this.creeps[i].position.y -= 1.7;
-					this.MOVE_S[i] = true;
-				}					
-				else if (this.creeps[i].position.y < yPathArray[this.creepWaypoint[i]] && this.MOVE_S[i] == false)
-				{
-					this.creeps[i].position.y += 1.7;
-					this.MOVE_N[i] = true;
-				}	
+					this.creepWaypoint[i] = xPathArray.length - 2;
+					this.creeps[i].position.x = xPathArray[xPathArray.length - 1];
+					this.creeps[i].position.y = yPathArray[xPathArray.length - 1];
+					this.MOVE_E[i] = false;
+					this.MOVE_W[i] = false;
+				}
 				else
 				{
-					this.creeps[i].position.y = yPathArray[this.creepWaypoint[i]];
-					
-					if (this.creeps[i].position.x == 1 && this.creeps[i].position.y == 1)
-					{
-						
-						this.creepWaypoint[i] = xPathArray.length - 2;
-						this.creeps[i].position.x = xPathArray[xPathArray.length - 1];
-						this.creeps[i].position.y = yPathArray[xPathArray.length - 1];
-						this.MOVE_N[i] = false;
-						this.MOVE_S[i] = false;
-					}
-					else
-					{
-						this.creepWaypoint[i]--;
-						this.MOVE_N[i] = false;
-						this.MOVE_S[i] = false;
-					}
+					this.creepWaypoint[i]--;
+					this.MOVE_E[i] = false;
+					this.MOVE_W[i] = false;
 				}
 			}
 		}
-		this.creeps[i].health -= .5
-		if (this.creeps[i].health <= 0)
+		
+		if (this.creeps[i].position.y != yPathArray[this.creepWaypoint[i]])
 		{
-			Creep.isdead();
+			if (this.creeps[i].position.y > yPathArray[this.creepWaypoint[i]] && this.MOVE_N[i] == false)
+			{
+				this.creeps[i].position.y -= 1.7;
+				this.MOVE_S[i] = true;
+			}					
+			else if (this.creeps[i].position.y < yPathArray[this.creepWaypoint[i]] && this.MOVE_S[i] == false)
+			{
+				this.creeps[i].position.y += 1.7;
+				this.MOVE_N[i] = true;
+			}	
+			else
+			{
+				this.creeps[i].position.y = yPathArray[this.creepWaypoint[i]];
+				
+				if (this.creeps[i].position.x == 1 && this.creeps[i].position.y == 1)
+				{
+					
+					this.creepWaypoint[i] = xPathArray.length - 2;
+					this.creeps[i].position.x = xPathArray[xPathArray.length - 1];
+					this.creeps[i].position.y = yPathArray[xPathArray.length - 1];
+					this.MOVE_N[i] = false;
+					this.MOVE_S[i] = false;
+				}
+				else
+				{
+					this.creepWaypoint[i]--;
+					this.MOVE_N[i] = false;
+					this.MOVE_S[i] = false;
+				}
+			}
 		}
-		info.innerHTML = 'Health: ' + this.creeps[i].health;
 	}
+	this.creeps[i].health -= .5
+	if (this.creeps[i].health <= 0)
+	{
+		Creep.isdead();
+	}
+	info.innerHTML = 'Health: ' + this.creeps[i].health;
 }
 
 Creep.isdead = function (i) {
