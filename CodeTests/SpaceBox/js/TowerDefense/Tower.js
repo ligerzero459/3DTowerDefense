a7f6d4466190ad60825ccf391f7d82ba045542a5
ligerzero459 <ligerzero459@gmail.com>
2011-11-02 17:32:03 -0500

    Some basic tower code written for the first time.
    Signed-off-by: ligerzero459 <ligerzero459@gmail.com>

---
+++ CodeTests/SpaceBox/js/TowerDefense/Tower.js
@@ -0,0 +1,11 @@
+var Tower = Tower || {};
+
+Tower.initialize = function () {
+	this.towers = [];
+	this.towerType = [{"type": "Tower", "color": 0xFFFF00, "geometry": "new THREE.CubeGeometry(80, 80, 80, 10, 10, 10)", "damage": 5, "firespeed": 10, "range": 3}, {"type": "Sniper", "color": 0x00CC00, "geometry": "new THREE.CubeGeometry(80, 80, 80, 10, 10, 10)", "damage": 15, "firespeed": 2, "range": 10}];
+	this.towerIndex = {"Tower": 0, "Sniper": 1};
+}
+
+Tower.create = function ( type, x, y ) {
+	var index = this.towerIndex[type];
+}
