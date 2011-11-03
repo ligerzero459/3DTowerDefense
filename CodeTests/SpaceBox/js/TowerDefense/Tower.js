f855b873f40d9480bce273fb429c5697f93ec055
ligerzero459 <ligerzero459@gmail.com>
2011-11-02 21:50:03 -0500

    Still trying to fix the creeps
    Signed-off-by: ligerzero459 <ligerzero459@gmail.com>

--- CodeTests/SpaceBox/js/TowerDefense/Tower.js
+++ CodeTests/SpaceBox/js/TowerDefense/Tower.js
@@ -5,7 +5,3 @@ Tower.initialize = function () {
 	this.towerType = [{"type": "Tower", "color": 0xFFFF00, "geometry": "new THREE.CubeGeometry(80, 80, 80, 10, 10, 10)", "damage": 5, "firespeed": 10, "range": 3}, {"type": "Sniper", "color": 0x00CC00, "geometry": "new THREE.CubeGeometry(80, 80, 80, 10, 10, 10)", "damage": 15, "firespeed": 2, "range": 10}];
 	this.towerIndex = {"Tower": 0, "Sniper": 1};
 }
-
-Tower.create = function ( x, y ) {
-	
-}
