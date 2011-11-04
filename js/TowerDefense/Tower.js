56b911f2d65ad476586a67502565106c42e68545
ligerzero459 <ligerzero459@gmail.com>
2011-11-04 03:50:03 -0500

    Beta 1.01 - Increased size of the tower placeholders
    Signed-off-by: ligerzero459 <ligerzero459@gmail.com>

--- js/TowerDefense/Tower.js
+++ js/TowerDefense/Tower.js
@@ -2,7 +2,7 @@ var Tower = Tower || {};
 
 Tower.initialize = function () {
 	this.towers = [];
-	this.towerType = [{"type": "Tower", "color": 0xFFFF00, "geometry": new THREE.CubeGeometry(80, 80, 80, 10, 10, 10), "damage": 5, "fireSpeed": 5, "range": 3, "shotPower": 100}, {"type": "Sniper", "color": 0x00CC00, "geometry": new THREE.CubeGeometry(80, 80, 80, 10, 10, 10), "damage": 15, "fireSpeed": 2, "range": 10, "shotPower": 100}];
+	this.towerType = [{"type": "Tower", "color": 0xFFFF00, "geometry": new THREE.CubeGeometry(90, 90, 90, 10, 10, 10), "damage": 5, "fireSpeed": 5, "range": 3, "shotPower": 100}, {"type": "Sniper", "color": 0x00CC00, "geometry": new THREE.CubeGeometry(90, 90, 90, 10, 10, 10), "damage": 15, "fireSpeed": 2, "range": 10, "shotPower": 100}];
 	this.towerIndex = {"Tower": 0, "Sniper": 1};
 }
 
