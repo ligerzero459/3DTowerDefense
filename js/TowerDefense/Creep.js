337dfff89aa0c9796b8f22b8a18e24e03762a70d
ligerzero459 <ligerzero459@gmail.com>
2011-11-04 03:43:28 -0500

    Main directory finished. Welcome to Beta 1.0!

--- js/TowerDefense/Creep.js
+++ js/TowerDefense/Creep.js
@@ -1,21 +1,118 @@
 var Creep = Creep || {};
 
 Creep.initialize = function () {
-	this.health = health;
-	this.x = xPathArray[xLength];
-	this.y = yPathArray[yLength];
+	this.pathLength = Map.xPathArray.length - 1
+	this.x = Map.xPathArray[this.pathLength];
+	this.y = Map.yPathArray[this.pathLength];
+	this.creeps = [];
+	this.creepWaypoint = [];
+	this.currentWave = 0;
+	this.wave = [{"color": 0x00FFFF, "health": 100, "amount": 10, "speed": 3.7, "spawnwait": 5000, "nextwave": 10000}, {"color": 0xFF0000, "health": 150, "amount": 10, "speed": 5, "spawnwait": 8000, "nextwave": 10000}];
 	
-	this.load();
 }
 
-Creep.load = function () {
-	var material, geometry, mesh;
-	material = new THREE.MeshLambertMaterial ( { color: 0x00FFFF } );
-	geometry = new THREE.SphereGeometry( 100, 20, 20 );
-	geometry.computeTangents();
-	mesh = new THREE.Mesh ( geometry, material );
-	mesh.position.set( this.x, this.y, 0 );
+Creep.runLevel = function() {
+	if (this.currentWave < ( this.wave.length )) {
+		if (this.wave[this.currentWave].amount > 0)
+		{
+			Creep.create(this.wave[this.currentWave].color, this.wave[this.currentWave].health, this.wave[this.currentWave].speed);
+			this.wave[this.currentWave].amount -= 1;
+			setTimeout("Creep.runLevel()", this.wave[this.currentWave].spawnwait);
+		}
+		else {
+			setTimeout("Creep.runLevel()", this.wave[this.currentWave].nextwave);
+			this.currentWave += 1;
+		}
+	}
+}
+
+Creep.create = function ( color, health, speed ) {
+	this.material = new THREE.MeshLambertMaterial ( { color: color } );
+	this.geometry = new THREE.SphereGeometry( 100, 20, 20 );
+	this.geometry.computeTangents();
+	this.mesh = new THREE.Mesh ( this.geometry, this.material );
+	this.mesh.position.set( this.x, this.y, 100 );
+	this.mesh.health = health;
+	this.mesh.speed = speed;
+	this.mesh.MOVE_N = false;
+	this.mesh.MOVE_S = false;
+	this.mesh.MOVE_E = false;
+	this.mesh.MOVE_W = false;
+	this.creeps.push ( this.mesh );
+	this.creepWaypoint.push ( this.pathLength - 1 );
 	
-	scene.add( mesh );
-	this.mesh = mesh;
+	scene.add( this.mesh );
+}
+
+Creep.update = function() {
+	for (var i in this.creeps)
+	{
+		if (this.creeps[i].position.x != Map.xPathArray[this.creepWaypoint[i]])
+		{
+			if (this.creeps[i].position.x > Map.xPathArray[this.creepWaypoint[i]] && this.creeps[i].MOVE_E == false)
+			{
+				this.creeps[i].position.x -= this.creeps[i].speed;
+				this.creeps[i].MOVE_W = true;
+			}
+			else if (this.creeps[i].position.x < Map.xPathArray[this.creepWaypoint[i]] && this.creeps[i].MOVE_W == false)
+			{
+				this.creeps[i].position.x += this.creeps[i].speed;
+				this.creeps[i].MOVE_E = true;
+			}
+			else
+			{
+				this.creeps[i].position.x = Map.xPathArray[this.creepWaypoint[i]];
+				this.creeps[i].position.y = Map.yPathArray[this.creepWaypoint[i]];
+				this.creepWaypoint[i]--;
+				this.creeps[i].MOVE_N = false;
+				this.creeps[i].MOVE_S = false;
+				this.creeps[i].MOVE_E = false;
+				this.creeps[i].MOVE_W = false;
+			}
+		}
+		else if (this.creeps[i].position.y != Map.yPathArray[this.creepWaypoint[i]])
+		{
+			if (this.creeps[i].position.y > Map.yPathArray[this.creepWaypoint[i]] && this.creeps[i].MOVE_N == false)
+			{
+				this.creeps[i].position.y -= this.creeps[i].speed;
+				this.creeps[i].MOVE_S = true;
+			}					
+			else if (this.creeps[i].position.y < Map.yPathArray[this.creepWaypoint[i]] && this.creeps[i].MOVE_S == false)
+			{
+				this.creeps[i].position.y += this.creeps[i].speed;
+				this.creeps[i].MOVE_N = true;
+			}
+			else
+			{
+				this.creeps[i].position.x = Map.xPathArray[this.creepWaypoint[i]];
+				this.creeps[i].position.y = Map.yPathArray[this.creepWaypoint[i]];
+				this.creepWaypoint[i]--;
+				this.creeps[i].MOVE_N = false;
+				this.creeps[i].MOVE_S = false;
+				this.creeps[i].MOVE_E = false;
+				this.creeps[i].MOVE_W = false;
+			}
+		}
+		else
+		{
+			this.creeps[i].position.x = Map.xPathArray[this.creepWaypoint[i]];
+			this.creeps[i].position.y = Map.yPathArray[this.creepWaypoint[i]];
+			this.creepWaypoint[i]--;
+			this.creeps[i].MOVE_N = false;
+			this.creeps[i].MOVE_S = false;
+			this.creeps[i].MOVE_E = false;
+			this.creeps[i].MOVE_W = false;
+		}
+	
+		info.innerHTML = 'Total Creeps Remaining: ' + this.wave[this.currentWave].amount;
+		if (this.creeps[i].health <= 0)
+		{
+			Creep.isDead(i);
+		}
+	}
+}
+
+Creep.isDead = function ( i ) {
+	scene.remove(this.creeps[i]);
+	this.creeps.splice(i, 1);
 }
---
+++ js/TowerDefense/Tower.js
@@ -0,0 +1,81 @@
+var Tower = Tower || {};
+
+Tower.initialize = function () {
+	this.towers = [];
+	this.towerType = [{"type": "Tower", "color": 0xFFFF00, "geometry": new THREE.CubeGeometry(80, 80, 80, 10, 10, 10), "damage": 5, "fireSpeed": 5, "range": 3, "shotPower": 100}, {"type": "Sniper", "color": 0x00CC00, "geometry": new THREE.CubeGeometry(80, 80, 80, 10, 10, 10), "damage": 15, "fireSpeed": 2, "range": 10, "shotPower": 100}];
+	this.towerIndex = {"Tower": 0, "Sniper": 1};
+}
+
+Tower.create = function( x, y, type ) {
+	this.material = new THREE.MeshLambertMaterial ( { color: this.towerType[type].color } );
+	this.geometry = this.towerType[type].geometry;
+	this.mesh = new THREE.Mesh ( this.geometry, this.material );
+	this.mesh.position.set( x, y, 100 );
+	this.mesh.shotPower = this.towerType[type].shotPower;
+	this.mesh.charge = 100;
+	this.mesh.fireSpeed = this.towerType[type].fireSpeed;
+	this.mesh.damage = this.towerType[type].damage;
+	this.mesh.range = this.towerType[type].range;
+	this.mesh.charging = false;
+	this.mesh.originalMaterial = new THREE.MeshLambertMaterial ( { color: this.towerType[type].color } );
+	this.towers.push( this.mesh );
+	
+	scene.add( this.mesh );
+}
+
+Tower.update = function() {
+	for (var i in this.towers)
+	{
+		if (this.towers[i].charging != true)
+			{
+			// Check if any targets are in range
+			var targets = Tower.creepsInRange(i);
+			
+			// If there are targets in range, select the one furthest along the track
+			if (targets != 0)
+			{
+				var target = targets[0];
+				var firingTower = this.towers[i];
+				Tower.hit(firingTower, target);
+				this.towers[i].charging = true;
+			}
+		}
+		else
+		{
+			this.towers[i].charge += this.towers[i].fireSpeed;
+			if (this.towers[i].charge >= 100)
+			{
+				this.towers[i].charging = false;
+			}
+		}
+	}
+}
+
+Tower.creepsInRange = function(i) {
+	var inRange = [];
+	var creepCounter = 0;
+	
+	for (var j in Creep.creeps) {
+		var xDistance = Creep.creeps[j].position.x - this.towers[i].position.x;
+		var yDistance = Creep.creeps[j].position.y - this.towers[i].position.y;
+		var Distance = Math.sqrt(Math.pow( xDistance, 2 ) + Math.pow( yDistance, 2 ));
+		
+		if ( Distance <= ( 200 * this.towers[i].range ) )
+		{
+			inRange.push( Creep.creeps[j] );
+			creepCounter++;
+		}
+	}
+	
+	if (creepCounter > 0) {
+		return inRange;
+	}
+	else {
+		return 0;
+	}
+}
+
+Tower.hit = function(firingTower, target) {
+	target.health -= firingTower.damage;
+	firingTower.charge -= firingTower.shotPower;	
+}
