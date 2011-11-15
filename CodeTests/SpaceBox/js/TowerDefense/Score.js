24b7bb79ef9a60147c98a9a74d1627e838ea8981
ligerzero459 <ligerzero459@gmail.com>
2011-11-14 19:33:07 -0600

    All towers unlocked
    Signed-off-by: ligerzero459 <ligerzero459@gmail.com>

--- CodeTests/SpaceBox/js/TowerDefense/Score.js
+++ CodeTests/SpaceBox/js/TowerDefense/Score.js
@@ -21,31 +21,39 @@ Score.getCash = function () {
 }
   
 Score.towerCheck = function () {
+	Tower.activate("Ultimate");
+	Tower.activate("Rapid");
+	Tower.activate("Fire");
+	Tower.activate("Splash");
+	Tower.activate("Sniper");
+	Tower.activate("Poison");
+	Tower.activate("Laser");
+	Tower.activate("Slow");
 	
-	if (this.myScore >= 50) {
-		Tower.activate("Slow");
+	/*if (this.myScore >= 500) {
+		Tower.activate("Ultimate");
 	}
-	else if (this.myScore >= 100) {
-		Tower.activate("Laser");
+	else if (this.myScore >= 400) {
+		Tower.activate("Rapid");
 	}
-	else if (this.myScore >= 150) {
-		Tower.activate("Poison");
+	else if (this.myScore >= 300) {
+		Tower.activate("Fire");
 	}
-	else if (this.myScore >= 200) {
-		Tower.activate("Sniper");
-		}
 	else if (this.myScore >= 250) {
 		Tower.activate("Splash");
 		}
-	else if (this.myScore >= 300) {
-		Tower.activate("Fire");
+	else if (this.myScore >= 200) {
+		Tower.activate("Sniper");
 		}
-	else if (this.myScore >= 400) {
-		Tower.activate("Rapid");
+	else if (this.myScore >= 150) {
+		Tower.activate("Poison");
 		}
-	else if (this.myScore >= 500) {
-		Tower.activate("Ultimate");
+	else if (this.myScore >= 100) {
+		Tower.activate("Laser");
 		}
+	else if (this.myScore >= 50) {
+		Tower.activate("Slow");
+		}*/
 }
 
 Score.buyTower = function(towerPrice) {
